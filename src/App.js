be90bb2aec4c661b38023901cc85918722bf841e
ErinaCeom <166990379+ErinaCeom@users.noreply.github.com>
2024-09-16 15:08:41 +0000

    react init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App border-2 border-black p-5">
+      <h1>hello world</h1>
+      <textarea className='bg-slate-600'></textarea>
+    </div>
+  );
+}
+
+export default App;
