cf3859c59984a1d89ce5691a068ee3b5d0754bc3
ErinaCeom <166990379+ErinaCeom@users.noreply.github.com>
2024-09-25 17:25:24 +0000

    words checking function done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
-import './app.css';
+import { useRef, useState } from "react";
+import "./style.css";
 
 function App() {
+  const targetInput = "hello world";
+  const [Result,setResult] = useState("");
+  let userInput = useRef("");
+
+  const checkWords = () => {
+    let userInputValue = userInput.current.value;
+
+    //check the last typed letter
+    if (
+      userInputValue[userInputValue.length - 1] ==
+      targetInput[userInputValue.length - 1]
+    ) {
+      console.log(true);
+      setResult(Result.concat("\ntrue"));
+    } else {
+      console.log(false);
+      setResult(Result.concat("\nfalse"));
+    }
+  };
+
   return (
-    <div className="App border-2 border-black p-5">
-      <h1>hello world</h1>
-      <textarea className='bg-slate-600'></textarea>
+    <div className="App">
+      <div className="inputs border-2 border-black rounded-md flex flex-col justify-center items-center p-5 m-5">
+        <input
+          type="text"
+          placeholder={targetInput}
+          disabled
+          className="border-2 border-gray-500 mt-2 w-2/4"
+        ></input>
+        <input
+          ref={userInput}
+          type="text"
+          placeholder="user text"
+          className="border-2 border-gray-500 mt-2 w-2/4"
+          onInput={checkWords}
+        ></input>
+        <textarea
+          value={Result}
+          className="border-2 border-black mt-4"
+        ></textarea>
+      </div>
     </div>
   );
 }
