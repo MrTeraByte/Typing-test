8833f5aaaafd3c2e68f06156b07598b4661a1009
ErinaCeom <166990379+ErinaCeom@users.noreply.github.com>
2024-10-05 13:27:25 +0000

    disabling backspace not working

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -4,27 +4,43 @@ import "../style/inputs.css";
 
 //have to fetch from other source
 export let targetInput =
-"Nikola Tesla, a famous inventor, and electrical engineer born in 1856. He made numerous groundbreaking contributions to the design of the modern alternating current (AC) electricity supply system.";
+  "Nikola Tesla, a famous inventor, and electrical engineer born in 1856. He made numerous groundbreaking contributions to the design of the modern alternating current (AC) electricity supply system.";
 
 export default function Inputs() {
   const invInput = useRef();
   const words = targetInput.split(" ");
 
   return (
-    <div className="target-text-container" onClick={() => {
-      invInput.current.focus();
-    }}>
-      {words.map((word) => {
+    <div
+      className="target-text-container"
+      onClick={() => {
+        invInput.current.focus();
+      }}
+    >
+      {words.map((word, index) => {
         return (
-          <div className="word-elem">
-            {word.split("").map((letter,index) => {
+          <div className="word-elem" key={index}>
+            {word.split("").map((letter, index) => {
               // console.log(letter);
-              return <div className="letter-elem" key={index}>{letter}</div>;
+              return (
+                <div className="letter-elem" key={index}>
+                  {letter}
+                </div>
+              );
             })}
           </div>
         );
       })}
-      <input className="invi-input" ref={invInput} onInput={(e) => handleInput(e)}></input>
+      <input
+        className="invi-input"
+        ref={invInput}
+        onInput={(e) => handleInput(e)}
+        onKeyDown={(e) => {
+          if (e.key === 'Backspace') {
+            e.preventDefault();
+          }
+        }}
+      ></input>
     </div>
   );
 }
